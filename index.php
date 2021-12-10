699d4228d1d324c632880e29ebbd01b422dd2c83
bea09bea <82158836+bea09bea@users.noreply.github.com>
2021-12-10 20:29:05 +0100

    homepage

--- index.php
+++ index.php
@@ -2,13 +2,41 @@
 <html>
     <head>
         <meta charset="utf-8">
-    	<link rel="stylesheet" href="css/style.css">
+    	<link rel="stylesheet" href="style.css">
 	    <title>Startsida</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
     </head>
     <body>
-        <div>Startsida</div>
+        <main class="homePage">
+            <div class="partOne">
+                <div class="homePageTitle">Välkommen till</div>
+                <div class="homePageTitle">Dog-go</div>
+                <div class="homePageImgOne"></div>
+                <div class="homePageImgTwo"></div>
+                <button class="homePagebuttonOne">Skapa konto</button>
+                <button class="homePagebuttonTwo">Logga in</button>
+            </div>
+
+            <div class="partTwo">
+                <div id="first">
+                    <div>Vad är Dog-go?</div>
+                    <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                    <button class="homePagebuttonThree">Skapa konto</button>
+                </div>
+
+                <div id="second">
+                    <div class="homePageImgThree"></div> 
+                </div>
+                
+            </div>
+
+        </main>
+
     </body>
 </html>
+
+<?php 
+require_once "section/footer.php";
+?>
