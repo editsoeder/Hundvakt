783e394f9b82dab9131d3eafe4694e37aa96d6f4
bea09bea <82158836+bea09bea@users.noreply.github.com>
2021-12-14 12:00:47 +0100

    tog bort lite send

--- dogsitter/update.php
+++ dogsitter/update.php
@@ -206,16 +206,17 @@ if ($method === "PATCH") {
 
                 //om days är = 0 tecken
                 if (strlen($requestData["days"]) == 0) {
-                    send([
-                        "code" => 401,
-                        "message" => "Bad request, invalid format",
-                        "errors" => [
-                                [
-                                    "field" => "days",
-                                    "message" => "`days` has to be more then 0 characters"
-                                ]
-                        ]
-                    ]); 
+                    echo "`days` has to be more then 0 characters";
+                    // send([
+                    //     "code" => 401,
+                    //     "message" => "Bad request, invalid format",
+                    //     "errors" => [
+                    //             [
+                    //                 "field" => "days",
+                    //                 "message" => "`days` has to be more then 0 characters"
+                    //             ]
+                    //     ]
+                    // ]); 
                 }
 
                 if (in_array($requestData["days"], $user["days"])) {
@@ -247,7 +248,7 @@ if ($method === "PATCH") {
     }
 
     saveJson("dogsitter.json", $dogSitter);
-    send($foundUser);
+    // send($foundUser);
 }
 
 ?>
