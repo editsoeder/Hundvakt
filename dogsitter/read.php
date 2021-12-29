b4aef6996708ffa1ffc11c920ca1a4036611cb97
editsoeder <82370421+editsoeder@users.noreply.github.com>
2021-12-29 13:48:42 +0100

    fixat in html-taggar i dogsitter.read

--- dogsitter/read.php
+++ dogsitter/read.php
@@ -1,96 +1,106 @@
-
 <?php 
 error_reporting(-1);
-// session_start();
-require_once __DIR__ . "/../section/header.php";
-require_once __DIR__ . "/../functions.php";
+session_start();
+?> 
 
-$allDogOwner = getAllDogOwner();
-// <title>Alla hundar</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alla hundar</title>
+    <?php 
+        include_once __DIR__ . "/../section/header2.php";
+    ?>
+<!-- </head> stängs istället i header.php --> 
+<body>
+    
+    <?php
+    require_once __DIR__ . "/../functions.php";
+    $allDogOwner = getAllDogOwner();
 
-//Om inloggad
-if(isset($_SESSION["loggedInAsDogSitter"])) {
+    //Om inloggad
+    if(isset($_SESSION["loggedInAsDogSitter"])) {
 
-    //  Om "id" finns i url
-    if (isset($_GET["id"])) {
-        $id = $_GET["id"];
+        //  Om "id" finns i url
+        if (isset($_GET["id"])) {
+            $id = $_GET["id"];
 
-       foreach($allDogOwner as $dogOwner){
-            if ($dogOwner["id_owner"] == $id) {
-                $foundDogOwner = $dogOwner; 
-                $dog = $dogOwner["dog"];
-            } 
-        }
+        foreach($allDogOwner as $dogOwner){
+                if ($dogOwner["id_owner"] == $id) {
+                    $foundDogOwner = $dogOwner; 
+                    $dog = $dogOwner["dog"];
+                } 
+            }
 
-        $src = '/Images/dogs.jpg';
-        // $src = '/Images/puppy.jpg';
+            $src = '/Images/dogs.jpg';
+            // $src = '/Images/puppy.jpg';
 
 
-        //Konvertera array till string
-        $days = implode(", ",$foundDogOwner["days"]);
+            //Konvertera array till string
+            $days = implode(", ",$foundDogOwner["days"]);
 
-        if (isset($foundDogOwner)) { 
-            $div = "
-            <img id='profileDog' src='$src' alt='Dog profil picture'>
-            <div class='one'>
-                <div class='dogName'>{$dog['dogName']}</div>
-                <div class='bold'>Ras: <p>{$dog['breed']}</p> </div>
-                <div class='bold'>Kön: <p>{$dog['gender']}</p></div>
-                <div class='bold'>Timkostnad: <p>{$foundDogOwner['cost']}</p></div>
-                <div class='bold'>Placering: <p>{$foundDogOwner['location']}</p></div>
-                <div class='bold'>Behöver hjälp: <p>{$days}</p></div>
+            if (isset($foundDogOwner)) { 
+                $div = "
+                <img id='profileDog' src='$src' alt='Dog profil picture'>
+                <div class='one'>
+                    <div class='dogName'>{$dog['dogName']}</div>
+                    <div class='bold'>Ras: <p>{$dog['breed']}</p> </div>
+                    <div class='bold'>Kön: <p>{$dog['gender']}</p></div>
+                    <div class='bold'>Timkostnad: <p>{$foundDogOwner['cost']}</p></div>
+                    <div class='bold'>Placering: <p>{$foundDogOwner['location']}</p></div>
+                    <div class='bold'>Behöver hjälp: <p>{$days}</p></div>
 
-            </div>
-
-            <div class='two'>
-                <p class='bold'>Kontaktas via:</p>
-                <p>{$foundDogOwner['email']}</p>
-            </div>
-
-            <div class='three'>
-                <p class='bold'>Bra att veta:</p>
-                <p>{$dog['extra']}</p>
-            </div>
-            ";
-
-            $content = "<div class='content'> $div</div>";
-            echo $content;
-            
-        } 
-
-    } elseif (!isset($_GET["id"])) {
-
-        $filter = '<div id="filterSitter"></div>'; 
-
-        $title = '
-        <div class="dogOwner"> 
-                <div class="listTitle"> 
-                    <div id="listName"> Namn</div>
-                    <div id="listName"> Placering</div>
-                    <div id="listName"> Dagar</div>
-                    <div id="listName"> Timlön</div>
                 </div>
-                <div class="list"></div>
-        </div>
-        ';
 
-        echo $filter; 
-        echo $title;
+                <div class='two'>
+                    <p class='bold'>Kontaktas via:</p>
+                    <p>{$foundDogOwner['email']}</p>
+                </div>
+
+                <div class='three'>
+                    <p class='bold'>Bra att veta:</p>
+                    <p>{$dog['extra']}</p>
+                </div>
+                ";
+
+                $content = "<div class='content'> $div</div>";
+                echo $content;
+                
+            } 
+
+        } elseif (!isset($_GET["id"])) {
+
+            $filter = '<div id="filterSitter"></div>'; 
+
+            $title = '
+            <div class="dogOwner"> 
+                    <div class="listTitle"> 
+                        <div id="listName"> Namn</div>
+                        <div id="listName"> Placering</div>
+                        <div id="listName"> Dagar</div>
+                        <div id="listName"> Timlön</div>
+                    </div>
+                    <div class="list"></div>
+            </div>
+            ';
+
+            echo $filter; 
+            echo $title;
+        }
     }
-}
 
-// Ej inloggad 
-    elseif(!isset($_SESSION["loggedInAsDogSitter"])) {
-    // header("Location: sign-out.php");
-}
+    // Ej inloggad 
+        elseif(!isset($_SESSION["loggedInAsDogSitter"])) {
+        header("Location: sign-out.php");
+    }
 
-?>
+    require_once __DIR__ . "/../section/footer.php"; ?>
+</body>
+</html>
 
 <script src="read.js"></script>
 
-<?php 
-require_once __DIR__ . "/../section/footer.php";
-?>
 
 
 
--- section/header2.php
+++ section/header2.php
@@ -1,29 +1,34 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../style.css">
 
-<header>
-    <div id="header-image"></div>
-</header>
+</head> 
 
-<?php
-    if (isset($_SESSION["loggedInAsDogOwner"])) {
-        echo '
-            <nav>
-                <a href="/dogowner/read.php">Hundvakter</a>
-                <a href/dogowner/profile.php">Profil</a>
-                <a href="../sign-out.php">Logga Ut</a>
-            </nav>';
-    }
-    
-    if (isset($_SESSION["loggedInAsDogSitter"])){
-        echo '
-            <nav>
-                <a href="/dogsitter/read.php">Hundar</a>
-                <a href="/dogsitter/profile.php">Profil</a>
-                <a href="../sign-out.php">Logga Ut</a>
-            </nav>';  
-        } else {
-           //vad ska det stå här? 
+<header> 
+    <div id="header-image"> </div>
+
+    <?php
+        if (isset($_SESSION["loggedInAsDogOwner"])) {
+            echo '
+                <nav>
+                    <a href="/dogowner/read.php">Hundvakter</a>
+                    <a href/dogowner/profile.php">Profil</a>
+                    <a href="../sign-out.php">Logga Ut</a>
+                </nav>';
         }
-?>
+        
+        if (isset($_SESSION["loggedInAsDogSitter"])){
+            echo '
+                <nav>
+                    <a href="/dogsitter/read.php">Hundar</a>
+                    <a href="/dogsitter/profile.php">Profil</a>
+                    <a href="../sign-out.php">Logga Ut</a>
+                </nav>';  
+            } else {
+            //vad ska det stå här? 
+            }
+    ?>
+</header>
