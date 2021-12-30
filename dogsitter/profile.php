3e434645b32230e6fbc7cd4a9ce7f0f626bd656b
qlaramarie <claramarielund@gmail.com>
2021-12-30 22:30:23 +0100

    profil css

--- dogsitter/profile.php
+++ dogsitter/profile.php
@@ -1,17 +1,6 @@
 <?php 
 error_reporting(-1);
 session_start();
-
-if (!isset($_SESSION["loggedInAsDogSitter"])) {
-    if(isset($_SESSION["loggedInAsDogOwner"])) {
-        header("Location: ../dogowner/profile.php");
-        exit();
-    } else {
-        header("Location: ../sign-in.php");
-        exit();
-    }
-}
-
 require_once __DIR__ . "/../functions.php";
 ?> 
 
@@ -55,7 +44,7 @@ require_once __DIR__ . "/../functions.php";
             <p>Du når mig på: <?php echo $sitterEmail ?> </p>
         </div>
         <div id="wrapper-extra_info">
-            <p>Bra att veta om mig är:<?php echo $sitterExtra?> </p> 
+            <p>Bra att veta om mig är:<br><?php echo $sitterExtra?> </p> 
         </div>
         <form action="update.php" method="PATCH">
             <button type="submit" id="change-settings-button">Ändra Uppgifter</button>
@@ -65,11 +54,7 @@ require_once __DIR__ . "/../functions.php";
     </div>
 
     
-    <script>
-        document.querySelector("#delete-account-button").addEventListener("click", function () {
-            window.location.href = "delete.php";
-        });
-    </script>
+    <script src="profile.js"></script>
 
     <?php
     require_once __DIR__ . "/../section/footer.php";
