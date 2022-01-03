7f12962bdc052371e1bbd1022e17e221d3da163b
qlaramarie <claramarielund@gmail.com>
2022-01-03 23:28:18 +0100

    Update.php

--- dogsitter/update.php
+++ dogsitter/update.php
@@ -40,12 +40,12 @@ $sitterPassword = $sitterInfo["password"];
     <div class="form">
         <form class="update-account" action="update.php" method="POST" enctype="multipart/form-data">
             <div id="dogsitter-form"> 
-                <p>Förnamn</p><input class="input-text" type="text" name="firstName" placeholder="<?php echo $sitterFirstName ?>"><br>
-                <p>Efternamn</p><input type="text" name="lastName" placeholder="<?php echo $sitterLastName ?>"><br>
-                <p>Email</p><input type="email" name="email" placeholder="<?php echo $sitterEmail ?>"><br>
-                <p>Nytt Lösenord</p><input type="password" name="password" placeholder ="<?php echo $sitterPassword ?>"placeholder="Skriv Nytt Lösenord"><br>
-                <p>Timkostnad</p><input type="text" name="cost" placeholder="<?php echo $sitterCost ?>"><p>kr/timm</p><br>
-                <p>Bra att veta</p><input type="text" name="extraInfo" placeholder="<?php echo $sitterExtra ?>"> <br> <br>
+                <p>Förnamn</p><input class="input-text" type="text" name="firstName" placeholder="<?php echo $sitterFirstName ?>" value ="<?php echo $sitterFirstName ?>">><br>
+                <p>Efternamn</p><input type="text" name="lastName" placeholder="<?php echo $sitterLastName ?>" value="<?php echo $sitterLastName ?>"><br>
+                <p>Email</p><input type="email" name="email" placeholder="<?php echo $sitterEmail ?>" value="<?php echo $sitterEmail ?>"><br>
+                <p>Lösenord</p><input type="password" name="password" placeholder="Skriv Nytt Lösenord" value="<?php echo $sitterPassword ?>" minlength="8" required><br>
+                <p>Timkostnad</p><input type="text" name="cost" placeholder="<?php echo $sitterCost ?>" value="<?php echo $sitterCost ?>"><p>kr/timm</p><br>
+                <p>Bra att veta</p><input type="text" name="extraInfo" placeholder="<?php echo $sitterExtra ?>" value="<?php echo $sitterExtra ?>"> <br> <br>
                 <div id="areaBoxUpdate">
                     <?php
                     createAreaBoxesUpdate();
@@ -114,40 +114,54 @@ $updateProfile = [
     "areas" => $_POST["areas"],
     "extraInfo" => $_POST["extraInfo"],
     "image" => $uniqueFilename.'.'.$extension //spara unika namnet på bilden som sökväg
-]  
-;  
+]; 
+
+// foreach($updateProfile as [$value=>$key]){
+//     $user = $data[$index];
+//     $user[$key] = $value;
+// }
+// foreach($data as $user){
+//     if($loggedInID === $user["id_sitter"]){
+//        $user = $updateProfile;
+//     }
+// }
+
+$column = array_column($data, "id_sitter");
+$index = array_search($loggedInID, $column);
+$user = $data[$index];
+$user = $updateProfile;
+
 foreach($data as $user){
     if($loggedInID === $user["id_sitter"]){
        $user = $updateProfile;
     }
-}
-$json = json_encode($data);
+} 
+// if(is_null($updateProfile ) ){
+//     echo "<p class 'feedbackMessageUpdate'> Något gick fel, försök igen </p>";
+//     exit();
+// }
+
+// if (empty($updateProfile ["first_name"]) || empty($updateProfile ["last_name"]) || empty($updateProfile ["email"]) || empty($updateProfile ["password"]) || empty($updateProfile ["location"]) || empty($updateProfile ["cost"]) || empty($updateProfile ["days"]) || empty($updateProfile ["areas"])|| empty($updateProfile ["extraInfo"])) {
+//     echo "<p class 'feedbackMessageUpdate'> Alla fält måste vara ifyllda, försök igen </p>";
+//     exit();
+// }
+
+// if(strlen($updateProfile ["password"]) < 4) {
+//     echo "<p class 'feedbackMessageUpdate'> Lösenord måste vara minst 4 tecken långt </p>";
+//     exit();       
+// }
+// if ($size > 4 * 1000 * 1000) {
+//     echo "Filen får inte vara större än 4mb";
+//     exit();
+// }
+
+$json = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents("dogsitter.json", $json);
 
-    if(is_null($updateProfile ) ){
-        echo "<p class 'feedbackMessageUpdate'> Något gick fel, försök igen </p>";
-        exit();
-    }
-
-    if (empty($updateProfile ["first_name"]) || empty($updateProfile ["last_name"]) || empty($updateProfile ["email"]) || empty($updateProfile ["password"]) || empty($updateProfile ["location"]) || empty($updateProfile ["cost"]) || empty($updateProfile ["days"]) || empty($updateProfile ["areas"])|| empty($updateProfile ["extraInfo"])) {
-        echo "<p class 'feedbackMessageUpdate'> Alla fält måste vara ifyllda, försök igen </p>";
-        exit();
-    }
-
-    if(strlen($updateProfile ["password"]) < 4) {
-        echo "<p class 'feedbackMessageUpdate'> Lösenord måste vara minst 4 tecken långt </p>";
-        exit();       
-    }
-    if ($size > 4 * 1000 * 1000) {
-        echo "Filen får inte vara större än 4mb";
-        exit();
-    }
 
 
 
-
-
-    // updateUser("dogsitter.json", $updateProfile);
+// updateUser("dogsitter.json", $updateProfile);
     // updateProfileSitter("../dogsitter.json", $updateProfile);
     echo "<p class 'feedbackMessageUpdate'> Profil Uppdaterad!</p>";
    }
